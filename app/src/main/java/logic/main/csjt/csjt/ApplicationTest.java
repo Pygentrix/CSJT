e98c2fe5690fd60a33f9ffaf82da15c3bf1bd280
Pygentrix <twelli@yahoo.de>
2016-04-02 18:03:12 +0200

    Initial Cardboard-App

---
+++ app/src/main/java/logic/main/csjt/csjt/ApplicationTest.java
@@ -0,0 +1,331 @@
+package logic.main.csjt.csjt;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class ApplicationTest extends CardboardActivity implements CardboardView.StereoRenderer  {
+
+    private static final String TAG = "CSJT";
+
+    private static final float Z_NEAR = 0.1f;
+    private static final float Z_FAR = 100.0f;
+
+    private static final float CAMERA_Z = 0.01f;
+    private static final float TIME_DELTA = 0.3f;
+
+    private static final float YAW_LIMIT = 0.12f;
+    private static final float PITCH_LIMIT = 0.12f;
+
+    // We keep the light always position just above the user.
+    private static final float[] LIGHT_POS_IN_WORLD_SPACE = new float[] {0.0f, 2.0f, 0.0f, 1.0f};
+
+    private static final float MIN_MODEL_DISTANCE = 3.0f;
+    private static final float MAX_MODEL_DISTANCE = 7.0f;
+
+    private final float[] lightPosInEyeSpace = new float[4];
+
+    private float[] camera;
+    private float[] view;
+    private float[] headView;
+    private float[] modelViewProjection;
+    private float[] modelView;
+
+    private float[] modelPosition;
+    private float[] headRotation;
+
+    private float objectDistance = MAX_MODEL_DISTANCE / 2.0f;
+    private Vibrator vibrator;
+
+    /**
+     * Converts a raw text file, saved as a resource, into an OpenGL ES shader.
+     *
+     * @param type The type of shader we will be creating.
+     * @param resId The resource ID of the raw text file about to be turned into a shader.
+     * @return The shader object handler.
+     */
+    private int loadGLShader(int type, int resId) {
+        String code = readRawTextFile(resId);
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, code);
+        GLES20.glCompileShader(shader);
+
+        // Get the compilation status.
+        final int[] compileStatus = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+        // If the compilation failed, delete the shader.
+        if (compileStatus[0] == 0) {
+            Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
+            GLES20.glDeleteShader(shader);
+            shader = 0;
+        }
+
+        if (shader == 0) {
+            throw new RuntimeException("Error creating shader.");
+        }
+
+        return shader;
+    }
+
+    /**
+     * Checks if we've had an error inside of OpenGL ES, and if so what that error is.
+     *
+     * @param label Label to report in case of error.
+     */
+    private static void checkGLError(String label) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, label + ": glError " + error);
+            throw new RuntimeException(label + ": glError " + error);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.common_ui);
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRenderer(this);
+        cardboardView.setTransitionViewEnabled(true);
+        cardboardView.setOnCardboardBackButtonListener(new Runnable() {
+            @Override
+            public void run() {
+                onBackPressed();
+            }
+        });
+        setCardboardView(cardboardView);
+
+        camera = new float[16];
+        view = new float[16];
+        modelViewProjection = new float[16];
+        modelView = new float[16];
+        // Model first appears directly in front of user.
+        modelPosition = new float[]{0.0f, 0.0f, -MAX_MODEL_DISTANCE / 2.0f};
+        headRotation = new float[4];
+        headView = new float[16];
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+        Log.i(TAG, "onRendererShutdown");
+    }
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {
+        Log.i(TAG, "onSurfaceChanged");
+    }
+
+    /**
+     * Creates the buffers we use to store information about the 3D world.
+     *
+     * <p>OpenGL doesn't use Java arrays, but rather needs data in a format it can understand.
+     * Hence we use ByteBuffers.
+     *
+     * @param config The EGL configuration used when creating the surface.
+     */
+    @Override
+    public void onSurfaceCreated(EGLConfig config) {
+        Log.i(TAG, "onSurfaceCreated");
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 0.5f); // Dark background so text shows up well.
+
+        checkGLError("onSurfaceCreated");
+    }
+
+    /**
+     * Updates the cube model position.
+     */
+    private void updateModelPosition() {
+
+    }
+
+    /**
+     * Converts a raw text file into a string.
+     *
+     * @param resId The resource ID of the raw text file about to be turned into a shader.
+     * @return The context of the text file, or null in case of error.
+     */
+    private String readRawTextFile(int resId) {
+        InputStream inputStream = getResources().openRawResource(resId);
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            reader.close();
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Prepares OpenGL ES before we draw a frame.
+     *
+     * @param headTransform The head transformation in the new frame.
+     */
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+        // Build the Model part of the ModelView matrix.
+        //Matrix.rotateM(modelCube, 0, TIME_DELTA, 0.5f, 0.5f, 1.0f);
+
+        // Build the camera matrix and apply it to the ModelView.
+        Matrix.setLookAtM(camera, 0, 0.0f, 0.0f, CAMERA_Z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+        headTransform.getHeadView(headView, 0);
+
+        // Update the 3d audio engine with the most recent head rotation.
+        headTransform.getQuaternion(headRotation, 0);
+
+        checkGLError("onReadyToDraw");
+    }
+
+    /**
+     * Draws a frame for an eye.
+     *
+     * @param eye The eye to render. Includes all required transformations.
+     */
+    @Override
+    public void onDrawEye(Eye eye) {
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        checkGLError("colorParam");
+
+        // Apply the eye transformation to the camera.
+        Matrix.multiplyMM(view, 0, eye.getEyeView(), 0, camera, 0);
+
+        // Set the position of the light
+        Matrix.multiplyMV(lightPosInEyeSpace, 0, view, 0, LIGHT_POS_IN_WORLD_SPACE, 0);
+
+        // Build the ModelView and ModelViewProjection matrices
+        // for calculating cube position and light.
+        float[] perspective = eye.getPerspective(Z_NEAR, Z_FAR);
+        //Matrix.multiplyMM(modelView, 0, view, 0, modelCube, 0);
+        Matrix.multiplyMM(modelViewProjection, 0, perspective, 0, modelView, 0);
+
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {}
+
+    /**
+     * Draw the cube.
+     *
+     * <p>We've set all of our transformation matrices. Now we simply pass them into the shader.
+     */
+    public void drawCube() {
+
+    }
+
+    /**
+     * Draw the floor.
+     *
+     * <p>This feeds in data for the floor into the shader. Note that this doesn't feed in data about
+     * position of the light, so if we rewrite our code to draw the floor first, the lighting might
+     * look strange.
+     */
+    public void drawFloor() {
+
+    }
+
+    /**
+     * Called when the Cardboard trigger is pulled.
+     */
+    @Override
+    public void onCardboardTrigger() {
+        Log.i(TAG, "onCardboardTrigger");
+
+        if (isLookingAtObject()) {
+           // hideObject();
+        }
+
+        // Always give user feedback.
+        vibrator.vibrate(50);
+    }
+
+    /**
+     * Find a new random position for the object.
+     *
+     * <p>We'll rotate it around the Y-axis so it's out of sight, and then up or down by a little bit.
+     */
+    private void hideObject() {
+        float[] rotationMatrix = new float[16];
+        float[] posVec = new float[4];
+
+        // First rotate in XZ plane, between 90 and 270 deg away, and scale so that we vary
+        // the object's distance from the user.
+        float angleXZ = (float) Math.random() * 180 + 90;
+        Matrix.setRotateM(rotationMatrix, 0, angleXZ, 0f, 1f, 0f);
+        float oldObjectDistance = objectDistance;
+        objectDistance =
+                (float) Math.random() * (MAX_MODEL_DISTANCE - MIN_MODEL_DISTANCE) + MIN_MODEL_DISTANCE;
+        float objectScalingFactor = objectDistance / oldObjectDistance;
+        Matrix.scaleM(rotationMatrix, 0, objectScalingFactor, objectScalingFactor, objectScalingFactor);
+        //Matrix.multiplyMV(posVec, 0, rotationMatrix, 0, modelCube, 12);
+
+        float angleY = (float) Math.random() * 80 - 40; // Angle in Y plane, between -40 and 40.
+        angleY = (float) Math.toRadians(angleY);
+        float newY = (float) Math.tan(angleY) * objectDistance;
+
+        modelPosition[0] = posVec[0];
+        modelPosition[1] = newY;
+        modelPosition[2] = posVec[2];
+
+        updateModelPosition();
+    }
+
+    /**
+     * Check if user is looking at object by calculating where the object is in eye-space.
+     *
+     * @return true if the user is looking at the object.
+     */
+    private boolean isLookingAtObject() {
+        float[] initVec = {0, 0, 0, 1.0f};
+        float[] objPositionVec = new float[4];
+
+        // Convert object space to camera space. Use the headView from onNewFrame.
+        //Matrix.multiplyMM(modelView, 0, headView, 0, modelCube, 0);
+        Matrix.multiplyMV(objPositionVec, 0, modelView, 0, initVec, 0);
+
+        float pitch = (float) Math.atan2(objPositionVec[1], -objPositionVec[2]);
+        float yaw = (float) Math.atan2(objPositionVec[0], -objPositionVec[2]);
+
+        return Math.abs(pitch) < PITCH_LIMIT && Math.abs(yaw) < YAW_LIMIT;
+    }
+}
