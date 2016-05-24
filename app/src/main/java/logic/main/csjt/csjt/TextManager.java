fa8c32118cd2fb775097e755d0286017a32b20ef
Jannik Brüggemann <brueggej@dhbw-loerrach.de>
2016-05-24 23:44:14 +0200

    New changes on class TextManager

--- app/src/main/java/logic/main/csjt/csjt/TextManager.java
+++ app/src/main/java/logic/main/csjt/csjt/TextManager.java
@@ -76,4 +76,210 @@ public class TextManager {
         texturenr = val;
     }
 
+    public void AddCharRenderInformation(float[] vec, float[] cs, float[] uv, short[] indi) {
+
+        short base = (short) (index_vecs / 3);
+
+        // We should add the vec, translating the indices to our saved vector
+        for (int i = 0; i < vec.length; i++) {
+            vecs[index_vecs] = vec[i];
+            index_vecs++;
+        }
+
+        // add the colors
+        for (int i = 0; i < cs.length; i++) {
+            colors[index_colors] = cs[i];
+            index_colors++;
+        }
+
+        // add the uvs
+        for (int i = 0; i < uv.length; i++) {
+            uvs[index_uvs] = uv[i];
+            index_uvs++;
+        }
+
+        // We handle the indices
+        for (int j = 0; j < indi.length; j++) {
+            indices[index_indices] = (short) (base + indi[j]);
+            index_indices++;
+        }
+    }
+
+    public void PrepareDrawInfo()
+    {
+        // Reset the indices.
+        index_vecs = 0;
+        index_indices = 0;
+        index_uvs = 0;
+        index_colors = 0;
+
+        // Get the total amount of characters
+        int charcount = 0;
+        for (TextObject txt : txtcollection) {
+            if(txt!=null)
+            {
+                if(!(txt.text==null))
+                {
+                    charcount += txt.text.length();
+                }
+            }
+        }
+
+        // Create the arrays we need with the correct size.
+        vecs = null;
+        colors = null;
+        uvs = null;
+        indices = null;
+
+        vecs = new float[charcount * 12];
+        colors = new float[charcount * 16];
+        uvs = new float[charcount * 8];
+        indices = new short[charcount * 6];
+
+    }
+
+    public void PrepareDraw()
+    {
+        // Setup all the arrays
+        PrepareDrawInfo();
+
+        // Using the iterator protects for problems with concurrency
+        for( Iterator< TextObject > it = txtcollection.iterator(); it.hasNext() ; )
+        {
+            TextObject txt = it.next();
+            if(txt!=null)
+            {
+                if(!(txt.text==null))
+                {
+                    convertTextToTriangleInfo(txt);
+                }
+            }
+        }
+    }
+
+    public void Draw(float[] m)
+    {
+        // Set the correct shader for our grid object.
+        GLES20.glUseProgram(riGraphicTools.sp_Text);
+
+        // The vertex buffer.
+        ByteBuffer bb = ByteBuffer.allocateDirect(vecs.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(vecs);
+        vertexBuffer.position(0);
+
+        // The vertex buffer.
+        ByteBuffer bb3 = ByteBuffer.allocateDirect(colors.length * 4);
+        bb3.order(ByteOrder.nativeOrder());
+        colorBuffer = bb3.asFloatBuffer();
+        colorBuffer.put(colors);
+        colorBuffer.position(0);
+
+        // The texture buffer
+        ByteBuffer bb2 = ByteBuffer.allocateDirect(uvs.length * 4);
+        bb2.order(ByteOrder.nativeOrder());
+        textureBuffer = bb2.asFloatBuffer();
+        textureBuffer.put(uvs);
+        textureBuffer.position(0);
+
+        // initialize byte buffer for the draw list
+        ByteBuffer dlb = ByteBuffer.allocateDirect(indices.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(indices);
+        drawListBuffer.position(0);
+
+        // get handle to vertex shader's vPosition member
+        int mPositionHandle = GLES20.glGetAttribLocation(riGraphicTools.sp_Text,
+                "vPosition");
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the background coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, 3,
+                GLES20.GL_FLOAT, false,
+                0, vertexBuffer);
+
+        int mTexCoordLoc = GLES20.glGetAttribLocation(riGraphicTools.sp_Text,
+                "a_texCoord" );
+
+        // Prepare the texturecoordinates
+        GLES20.glVertexAttribPointer ( mTexCoordLoc, 2, GLES20.GL_FLOAT,
+                false,
+                0, textureBuffer);
+
+        GLES20.glEnableVertexAttribArray ( mPositionHandle );
+        GLES20.glEnableVertexAttribArray ( mTexCoordLoc );
+
+        int mColorHandle = GLES20.glGetAttribLocation(riGraphicTools.sp_Text,
+                "a_Color");
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(mColorHandle);
+
+        // Prepare the background coordinate data
+        GLES20.glVertexAttribPointer(mColorHandle, 4,
+                GLES20.GL_FLOAT, false,
+                0, colorBuffer);
+
+        // get handle to shape's transformation matrix
+        int mtrxhandle = GLES20.glGetUniformLocation(riGraphicTools.sp_Text,
+                "uMVPMatrix");
+
+        // Apply the projection and view transformation
+        GLES20.glUniformMatrix4fv(mtrxhandle, 1, false, m, 0);
+
+        int mSamplerLoc = GLES20.glGetUniformLocation (riGraphicTools.sp_Text,
+                "s_texture" );
+
+        // Set the sampler texture unit to our selected id
+        GLES20.glUniform1i ( mSamplerLoc, texturenr);
+
+        // Draw the triangle
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, indices.length,
+                GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+        GLES20.glDisableVertexAttribArray(mTexCoordLoc);
+        GLES20.glDisableVertexAttribArray(mColorHandle);
+
+    }
+
+    private int convertCharToIndex(int c_val)
+    {
+        int indx = -1;
+
+        // Retrieve the index
+        if(c_val>64&&c_val<91) // A-Z
+            indx = c_val - 65;
+        else if(c_val>96&&c_val<123) // a-z
+            indx = c_val - 97;
+        else if(c_val>47&&c_val<58) // 0-9
+            indx = c_val - 48 + 26;
+        else if(c_val==43) // +
+            indx = 38;
+        else if(c_val==45) // -
+            indx = 39;
+        else if(c_val==33) // !
+            indx = 36;
+        else if(c_val==63) // ?
+            indx = 37;
+        else if(c_val==61) // =
+            indx = 40;
+        else if(c_val==58) // :
+            indx = 41;
+        else if(c_val==46) // .
+            indx = 42;
+        else if(c_val==44) // ,
+            indx = 43;
+        else if(c_val==42) // *
+            indx = 44;
+        else if(c_val==36) // $
+            indx = 45;
+
+        return indx;
+    }
 }
