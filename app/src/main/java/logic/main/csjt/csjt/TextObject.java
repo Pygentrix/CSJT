390dc28ea898934416fdffd5dbcb279bce81e6a1
Jannik Brüggemann <brueggej@dhbw-loerrach.de>
2016-05-24 21:50:18 +0200

    Changes on class TextObject

--- app/src/main/java/logic/main/csjt/csjt/TextObject.java
+++ app/src/main/java/logic/main/csjt/csjt/TextObject.java
@@ -3,6 +3,25 @@ package logic.main.csjt.csjt;
 /**
  * Created by JB on 23.05.2016.
  */
-public class TextObject {
-    
+public class TextObject { //this class holds the text, coordinates and the color as variables
+    public String text;
+    public float x;
+    public float y;
+    public float[] color;
+
+    public TextObject()
+    {
+        text = "This is a cube";
+        x = 0f;
+        y = 0f;
+        color = new float[] {1f, 1f, 1f, 1.0f};
+    }
+
+    public TextObject(String txt, float xcoord, float ycoord)
+    {
+        text = txt;
+        x = xcoord;
+        y = ycoord;
+        color = new float[] {1f, 1f, 1f, 1.0f};
+    }
 }
