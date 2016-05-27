454ee540e1b6ebcfc8e547596d19ecdc65bb361e
Jannik Brüggemann <brueggej@dhbw-loerrach.de>
2016-05-27 17:53:20 +0200

    Cuboid testing

--- app/src/main/java/logic/main/csjt/csjt/ApplicationTest.java
+++ app/src/main/java/logic/main/csjt/csjt/ApplicationTest.java
@@ -53,6 +53,7 @@ public class ApplicationTest extends CardboardActivity implements CardboardView.
     Cube cube2;
     Cube light1;
     Cuboid cuboid1;
+    Cuboid cuboid2;
     ///////////////////////////////////////
     Cube[][] cubes = new Cube[m][m];
     ///////////////////////////////////////
@@ -65,6 +66,7 @@ public class ApplicationTest extends CardboardActivity implements CardboardView.
 
         tetra1 = new Tetrahedron(-5.0f,1.0f,3.0f,1.5f,1.5f,1.5f, 1.0f, 0.8f, 0.0f, 1.0f);
         cuboid1 = new Cuboid(5.0f,5.0f,8.0f,6.0f,3.7f,8.7f, 1.4f, 2.6f, 0.4f, 0.2f);
+        cuboid2 = new Cuboid(-10.0f,8.0f,4.0f,6.0f,3.7f,0.7f, 1.0f, 0.5f, 0.4f, 1.0f);
 
         cube1 = new Cube(-5.0f,1.0f,5.0f,0.5f,0.5f,0.5f, 1.0f, 0.6523f, 0.0f, 1.0f);
         cube2 = new Cube(1.0f,8.0f,3.0f,0.7f,0.7f,0.7f, 1.0f, 0.5f, 0.4f, 1.0f);
@@ -225,12 +227,14 @@ public class ApplicationTest extends CardboardActivity implements CardboardView.
 
         tetra1.initProgram(vertexShader,passthroughShader);
         cuboid1.initProgram(vertexShader, passthroughShader);
+        cuboid2.initProgram(vertexShader, passthroughShader);
         cube1.initProgram(vertexShader,passthroughShader); //<- Program for every single cube or one for all ?
         cube2.initProgram(vertexShader,passthroughShader);
         light1.initProgram(vertexShader,passthroughShader);
 
         tetra1.updateModelPosition();
         cuboid1.updateModelPosition();
+        cuboid2.updateModelPosition();
 
         light1.updateLightPosition();
         cube1.updateModelPosition();
@@ -323,6 +327,7 @@ public class ApplicationTest extends CardboardActivity implements CardboardView.
 
         tetra1.draw(lightPosInEyeSpace, view, perspective);
         cuboid1.draw(lightPosInEyeSpace, view, perspective);
+        cuboid2.draw(lightPosInEyeSpace, view, perspective);
 
         if(cube1.isLookingAtObject(headView)){
             cube1.movY = 0.25f;
