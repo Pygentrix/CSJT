20386a10475a389fc954198b481b3e41cbe319c7
Jannik Brüggemann <brueggej@dhbw-loerrach.de>
2016-06-02 11:03:05 +0200

    Changes on class Cylinder

--- app/src/main/java/logic/main/csjt/csjt/Cylinder.java
+++ app/src/main/java/logic/main/csjt/csjt/Cylinder.java
@@ -3,44 +3,100 @@ package logic.main.csjt.csjt;
 /**
  * Created by JB on 31.05.2016.
  */
-public class Cylinder extends Geom{
-    /*
-    public float[] setCylinderCoords(float px,float py,float pz ,float width, float height, float depth){
+public class Cylinder extends JustForCylinder{
 
-    float steps = (float) ((Math.PI * 2) / parts);
+    private static final double TWOPI = 2 * Math.PI;
+    //Anzahl der Scheiben des Rings
+    int slices;
+    //Radius
+    double radius;
+    private float[] verticesAux = new float[3];
+    //Höhe
+    private double height;
+    //Radius der Basis des Zylinders, wenn =0 erhält man einen Kegel, wenn =1 erhält man einen Zylinder
+    private double base;
 
-    vertex = new float[parts * 6 + 12];
-
-
-
-    for (int i = 0; i <= parts; i++) {
-        float sin = (float) Math.sin(i * steps);
-        float cos = (float) Math.cos(i * steps);
-// Hintere Seite des Zylinders
-        vertex[i * 3] = cos;
-        vertex[i * 3 + 1] = sin;
-        vertex[i * 3 + 2] = -0.5f;
-// Vordere Seite des Zylinders
-        vertex[i * 3 + parts * 3 + 3] = cos;
-        vertex[i * 3 + parts * 3 + 4] = sin;
-        vertex[i * 3 + parts * 3 + 5] = 0.5f;
+    public Cylinder(){
+        super();
+        height = 2;
+        slices = 5;
+        radius = 1;
+        base = 1;
+        initBuffAndPutV();
     }
-// Mitte der beiden Kreise
-    vertex[parts * 6 + 6] = 0;
-    vertex[parts * 6 + 7] = 0;
-    vertex[parts * 6 + 8] = -0.5f;
-    vertex[parts * 6 + 9] = 0;
-    vertex[parts * 6 + 10] = 0;
-    vertex[parts * 6 + 11] = 0.5f;
 
-    // Buffer für die Punkte
-ByteBuffer vbb = ByteBuffer.allocateDirect(vertex.length *
-4);
-vbb.order(ByteOrder.nativeOrder());
-vertexBuffer = vbb.asFloatBuffer();
-vertexBuffer.put(vertex);
-vertexBuffer.position(0);
-gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+    public Cylinder(double base, double radius, double height, int slices){
+        super();
+        this.radius = radius > 0.0 ? radius : 0.1;
+        this.height = height > 0.0 ? height : 0.1;
+        this.slices = slices > 2 ? slices : 3;
+        this.base = base >= 0.0 ? base : 0.0;
+        initBuffAndPutV();
+    }
 
-*/
+    private void initBuffAndPutV() {
+        vertexNumber = slices *2+2;
+        bufferSize = slices * slices * 4 * 4;
+        initBuffer();
+        putVertex();
+    }
+
+    @Override
+    public void putVertex() {
+        double Theta = 0;
+        for (int i = 0; i < slices; i++) {
+
+            for (int j = 0; j < 2; j++)
+            {
+                verticesAux[0] = (float) (base * radius * Math.cos(Theta));
+                verticesAux[1] = (float) (0);
+                verticesAux[2] = (float) (base * radius * Math.sin(Theta));
+                vertexBuffer.put(verticesAux);
+
+                verticesAux[0] = (float) (radius * Math.cos(Theta));
+                verticesAux[1] = (float) (height);
+                verticesAux[2] = (float) (radius * Math.sin(Theta));
+                vertexBuffer.put(verticesAux);
+
+                Theta = Theta + (TWOPI / slices);
+            }
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius > 0.0 ? radius : 0.1;
+        initBuffAndPutV();
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height > 0.0 ? height : 0.1;
+        initBuffAndPutV();
+    }
+
+    public int getSlices() {
+        return slices;
+    }
+
+    public void setSlices(int slices) {
+        this.slices = slices > 2 ? slices : 3;
+        initBuffAndPutV();
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base >= 0.0 ? base : 0.0;
+        initBuffAndPutV();
+    }
 }
+
