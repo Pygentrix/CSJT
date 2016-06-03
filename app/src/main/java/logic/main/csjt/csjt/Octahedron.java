7545255ba8c5ac29643cb0b740a3cf88eae5ad89
Jannik Brüggemann <brueggej@dhbw-loerrach.de>
2016-06-03 22:38:38 +0200

    Set the Octahedron normals - second comments just for me

--- app/src/main/java/logic/main/csjt/csjt/Octahedron.java
+++ app/src/main/java/logic/main/csjt/csjt/Octahedron.java
@@ -52,4 +52,69 @@ public class Octahedron extends Geom{
 
         return OCTA_COORDS;
     }
+
+    public float[] setOctaNormals(float width, float height, float depth){
+
+        //Normals are constructed similar to the coordinates
+        float[] OCTA_NORMALS = new float[]{
+                //Bottom
+              //  0.0f,       -height,    0.0f,       //back left point
+              //  0.0f,       -height,    0.0f,       //front left point
+              //  0.0f,       -height,    0.0f,       //back right point
+
+              //  0.0f,       -height,    0.0f,       //front left point
+              //  0.0f,       -height,    0.0f,       //back right point
+              //  0.0f,       -height,    0.0f,       //front right point
+
+                //Back Top
+                0.0f,       height,     -2*depth,   //back left point //BLUE
+                0.0f,       height,     -2*depth,   //back right point //RED
+                0.0f,       height,     -2*depth,   //top //ORANGE
+
+                //Left Top
+                -2*width,   height,     0.0f,       //back left point //BLUE
+                -2*width,   height,     0.0f,       //front left point //YELLOW
+                -2*width,   height,     0.0f,       //top //ORANGE
+
+                //Front Top
+                0.0f,       height,     2*depth,    //front left point //YELLOW
+                0.0f,       height,     2*depth,    //front right point //GREEN
+                0.0f,       height,     2*depth,    //top //ORANGE
+
+                //Right Top
+                2*width,    height,     0.0f,       //back right point //RED
+                2*width,    height,     0.0f,       //front right point //GREEN
+                2*width,    height,     0.0f,       //top //ORANGE
+
+                //Back Bottom
+                0.0f,       height,     -2*depth,   //back left point //BLUE
+                0.0f,       height,     -2*depth,   //back right point //RED
+                0.0f,       height,     -2*depth,   //top //ORANGE
+
+                //Left Bottom
+                -2*width,   height,     0.0f,       //back left point //BLUE
+                -2*width,   height,     0.0f,       //front left point //YELLOW
+                -2*width,   height,     0.0f,       //top //ORANGE
+
+                //Front Bottom
+                0.0f,       height,     2*depth,    //front left point //YELLOW
+                0.0f,       height,     2*depth,    //front right point //GREEN
+                0.0f,       height,     2*depth,    //top //ORANGE
+
+                //Right Bottom
+                2*width,    height,     0.0f,       //back right point //RED
+                2*width,    height,     0.0f,       //front right point //GREEN
+                2*width,    height,     0.0f,       //top //ORANGE
+
+        };
+
+        return OCTA_NORMALS;
+    }
+
+    public Octahedron(Float x, Float y, Float z) {
+
+        float r, g, b, a = 1.0f;
+
+    }
+
 }
